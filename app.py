4d27d35f9f3cfa43759b147d188f3d69fe521bf5
oha95 <39189149+oha95@users.noreply.github.com>
2019-09-03 17:57:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from flask import Flask
-from flask_restful import reqparse, abort, Api, Resource
+# -*- coding: utf-8 -*-
 
-app = Flask(__name__)
-api = Api(app)
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
 
-class Team(Resource):
-    def get(self):
-        return {
-            'team': ['oussama', 'yacine', 'ludovic']
-        }
+from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+import re
 
-api.add_resource(Team, '/')
+LANGUAGE = "french"
+SENTENCES_COUNT = 15
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+
+if __name__ == "__main__":
+    url = "https://fr.wikipedia.org/wiki/Napol%C3%A9on_Ier"
+    parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
+    stemmer = Stemmer(LANGUAGE)
+
+    summarizer = Summarizer(stemmer)
+    summarizer.stop_words = get_stop_words(LANGUAGE)
+
+    for sentence in summarizer(parser.document, SENTENCES_COUNT):
+        print(sentence)
