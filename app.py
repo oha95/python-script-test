0f6e234ffab9182e34ab10dcb2a27c876d1e76dc
oha95 <39189149+oha95@users.noreply.github.com>
2019-09-03 18:04:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,6 @@
-from __future__ import absolute_import
-from __future__ import division, print_function, unicode_literals
+!pip install pyteaser 
+from pyteaser import SummarizeUrl
 
-from sumy.parsers.html import HtmlParser
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lex_rank import LexRankSummarizer as Summarizer
-from sumy.nlp.stemmers import Stemmer
-from sumy.utils import get_stop_words
-
-
-LANGUAGE = "french"
-SENTENCES_COUNT = 15
-
-
-if __name__ == "__main__":
-    url = "https://fr.wikipedia.org/wiki/Napol%C3%A9on_Ier"
-    parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
-  
-    stemmer = Stemmer(LANGUAGE)
-
-    summarizer = Summarizer(stemmer)
-    summarizer.stop_words = get_stop_words(LANGUAGE)
-
-    for sentence in summarizer(parser.document, SENTENCES_COUNT):
-        print(sentence)
+url = 'https://fr.wikipedia.org/wiki/Napol%C3%A9on_Ier'
+summaries = SummarizeUrl(url)
+print (summaries)
