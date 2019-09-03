aa2d52babaceee4363541210fedc85d57034742c
oha95 <39189149+oha95@users.noreply.github.com>
2019-09-03 18:00:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-# -*- coding: utf-8 -*-
-
 from __future__ import absolute_import
 from __future__ import division, print_function, unicode_literals
 
 from sumy.parsers.html import HtmlParser
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.summarizers.lex_rank import LexRankSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
-import re
+
 
 LANGUAGE = "french"
 SENTENCES_COUNT = 15
@@ -18,6 +16,7 @@ SENTENCES_COUNT = 15
 if __name__ == "__main__":
     url = "https://fr.wikipedia.org/wiki/Napol%C3%A9on_Ier"
     parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
+  
     stemmer = Stemmer(LANGUAGE)
 
     summarizer = Summarizer(stemmer)
