214a737ac5785e793a6fc9310f1c8f6f48858883
oha95 <39189149+oha95@users.noreply.github.com>
2019-09-04 11:18:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,73 +1,80 @@
-import bs4 as bs 
-import lxml
-import urllib.request  
-import re
-import heapq  
-
+import numpy as np
+import pandas as pd
 import nltk
+import re
 from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
-nltk.download('punkt')
-nltk.download('stopwords')
+stop_words = stopwords.words('english')
+from sklearn.metrics.pairwise import cosine_similarity
+import networkx as nx
+import bs4 as bs  
+import urllib.request  
 
-# Dataset
-uri='https://fr.wikipedia.org/wiki/Napol%C3%A9on_Ier'
+df = ['https://fr.wikipedia.org/wiki/Napol%C3%A9on_Ier']
 
-# Retrieving text from Html paragraphs
-scraped_data = urllib.request.urlopen(uri)  
-article = scraped_data.read()
-parsed_article = bs.BeautifulSoup(article,'lxml')
-paragraphs = parsed_article.find_all('p')
+# Extract word vectors
+word_embeddings = {}
+f = open('glove.6B.100d.txt', encoding='utf-8')
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    word_embeddings[word] = coefs
+f.close()
 
-def summarize(article_text, val):
-  summary_sentences = []
+# function to remove stopwords
+def remove_stopwords(sen):
+    sen_new = " ".join([i for i in sen if i not in stop_words])
+    return sen_new
+  
+sentences = []
+for s in df:
+  print(s)
+  scraped_data = urllib.request.urlopen(s)  
+  article = scraped_data.read()
+  parsed_article = bs.BeautifulSoup(article,'lxml')
+  paragraphs = parsed_article.find_all('p')
+  article_text = ""
+  for p in paragraphs:  
+    article_text += p.text
     
   # Removing Square Brackets and Extra Spaces
   article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)  
   article_text = re.sub(r'\s+', ' ', article_text)  
+  sentences.append(sent_tokenize(article_text))
 
-  # Removing special characters and digits
-  formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )  
-  formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)  
+sentences = [y for x in sentences for y in x] # flatten list
 
-  if not len(formatted_article_text) <= 1: 
+# remove punctuations, numbers and special characters
+clean_sentences = pd.Series(sentences).str.replace("[^a-zA-Z]", " ")
 
-    # Tokenizing content
-    sentence_list = nltk.sent_tokenize(article_text)  
-    stopwords = nltk.corpus.stopwords.words('french')
+# make alphabets lowercase
+clean_sentences = [s.lower() for s in clean_sentences]
 
-    word_frequencies = {}  
-    for word in nltk.word_tokenize(formatted_article_text):  
-        if word not in stopwords:
-            if word not in word_frequencies.keys():
-                word_frequencies[word] = 1
-            else:
-                word_frequencies[word] += 1
+# remove stopwords from the sentences
+clean_sentences = [remove_stopwords(r.split()) for r in clean_sentences]
 
-    maximum_frequncy = max(word_frequencies.values())
+sentence_vectors = []
+for i in clean_sentences:
+  if len(i) != 0:
+    v = sum([word_embeddings.get(w, np.zeros((100,))) for w in i.split()])/(len(i.split())+0.001)
+  else:
+    v = np.zeros((100,))
+  sentence_vectors.append(v)
+  
+# similarity matrix
+sim_mat = np.zeros([len(sentences), len(sentences)])
 
-    for word in word_frequencies.keys():  
-        word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+for i in range(len(sentences)):
+  for j in range(len(sentences)):
+    if i != j:
+      sim_mat[i][j] = cosine_similarity(sentence_vectors[i].reshape(1,100), sentence_vectors[j].reshape(1,100))[0,0]
+      
+nx_graph = nx.from_numpy_array(sim_mat)
+scores = nx.pagerank(nx_graph)
 
-    sentence_scores = {}  
-    for sent in sentence_list:  
-        for word in nltk.word_tokenize(sent.lower()):
-            if word in word_frequencies.keys():
-                if len(sent.split(' ')) < 30:
-                    if sent not in sentence_scores.keys():
-                        sentence_scores[sent] = word_frequencies[word]
-                    else:
-                        sentence_scores[sent] += word_frequencies[word]
+ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)
 
-    summary_sentences = heapq.nlargest(val, sentence_scores, key=sentence_scores.get)
-  return summary_sentences
-
-# 1st Loop to summarize paragraphs
-tmp = []
-for p in paragraphs:  
-  s = summarize(p.text, 1)
-  tmp = tmp + s
-
-# 2nd Loop to select sentences
-summary = ""
-print('\n'.join(summarize('\n'.join(tmp), 15)))
+# Extract top 10 sentences as the summary
+for i in range(10):
+  print(ranked_sentences[i][1])
