8e4d744e31a70c5e52b28006f5f9c6a219f857d2
oha95 <39189149+oha95@users.noreply.github.com>
2019-09-10 12:16:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -54,8 +54,8 @@ class TodoList(Resource):
 ##
 ## Actually setup the Api resource routing here
 ##
-api.add_resource(TodoList, '/todos')
-api.add_resource(Todo, '/todos/<todo_id>')
+api.add_resource(TodoList, '/')
+api.add_resource(Todo, '/<todo_id>')
 
 # Run the application
 if __name__ == '__main__':
